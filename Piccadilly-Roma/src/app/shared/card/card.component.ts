4830cacd4eb68b580d54f35234f8ca0c05869d3a
Matteo Giuntella <matteo.giuntella@intellitronika.com>
2025-05-16 17:37:04 +0200

    add new template card

--- Piccadilly-Roma/src/app/shared/card/card.component.ts
+++ Piccadilly-Roma/src/app/shared/card/card.component.ts
@@ -5,22 +5,130 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-      <div class="container d-flex align-items-center justify-content-center position-relative flex-wrap">
-        <div class="card d-flex position-relative flex-column">
-            <div class='imgContainer'>
-                <img src='assets/img/vetrina.png'>
+    <div class="container h-100">
+      <div class="row align-middle">
+        <div class="col-md-6 col-lg-4 column">
+          <div class="card gr-1">
+            <div class="txt">
+              <h1>
+                BRANDING AND <br />
+                CORPORATE DESIGN
+              </h1>
+              <p>Visual communication and problem-solving</p>
             </div>
-            <div class="content">
-                <h2>Piccadilly</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh.  Morbi in leo in eros commodo volutpat ac sed dolor.</p>
+            <a href="#">more</a>
+            <div class="ico-card">
+              <i class="fa fa-rebel"></i>
             </div>
+          </div>
         </div>
-      </div>  
+      </div>
+    </div>
   `,
   styles: `
+ 
+$gr-1:linear-gradient(170deg, #01E4F8 0%, #1D3EDE 100%);
 
- `
-})
-export class CardComponent {
 
+.gr-1{background: $gr-1;}
+
+
+*{transition: .5s;}
+
+.h-100{height: 100vh !important;}
+.align-middle{
+  position: relative;
+  top:50%;
+  transform:translateY(-50%);
 }
+
+.column{
+  margin-top:3rem;
+  padding-left:3rem;
+  &:hover{
+    padding-left:0;
+    .card .txt{
+      margin-left:1rem;
+      h1, p{
+        color:rgba(255,255,255,1);
+        opacity:1;
+      }
+    }
+    a{
+      color:rgba(255,255,255,1);
+      &:after{
+      width: 10%;
+      }
+    }
+  }
+}
+.card{
+  min-height:170px;
+  margin: 0;
+  padding: 1.7rem 1.2rem;
+  border: none;
+  border-radius: 0;
+  color:rgba(0,0,0,1);
+  letter-spacing: .05rem;
+  font-family: 'Oswald', sans-serif;
+  box-shadow: 0 0 21px rgba(0,0,0,.27);
+  .txt{
+    margin-left:-3rem;
+    z-index: 1;
+    h1{
+      font-size:1.5rem;
+      font-weight: 300;
+      text-transform: uppercase;
+    }
+    p{
+      font-size:.7rem;
+      font-family: 'Open Sans', sans-serif;
+      letter-spacing: 0rem;
+      margin-top:33px;
+      opacity:0;
+      color:rgba(255,255,255,1);
+    }
+  }
+  a{
+    z-index:3;
+    font-size: .7rem;
+    color:rgba(0,0,0,1);
+    margin-left:1rem;
+    position:relative;
+    bottom: -.5rem;
+    text-transform: uppercase;
+    &:after {
+      content:"";
+      display: inline-block;
+      height: 0.5em;
+      width: 0;
+      margin-right: -100%;
+      margin-left: 10px;
+      border-top: 1px solid rgba(255,255,255,1);
+      transition: .5s;
+    }
+  }
+  .ico-card{
+    position:absolute;
+    top: 0;
+    left:0;
+    bottom:0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  i{
+    position: relative;
+    right: -50%;
+    top:60%;
+    font-size: 12rem;
+    line-height: 0;
+    opacity: .2;
+    color:rgba(255,255,255,1);
+    z-index: 0;
+   }
+}
+ `,
+})
+export class CardComponent {}
